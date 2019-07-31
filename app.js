ea6d611f750e12d2294cae51f94bbac9342e91e0
root <root@ip-172-31-16-162.ap-south-1.compute.internal>
2019-07-31 13:22:14 +0000

    Jenkin test

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.get('/getdata', function(req, res){
+	console.log("Jenkins @ ah06701")
+	res.send("Test check")
+})
+
+app.listen(4000);
+
